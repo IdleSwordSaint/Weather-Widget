3ca2797567b0a441096d7e7d67afcee1560f3d1c
IdleSwordSaint <144363866+IdleSwordSaint@users.noreply.github.com>
2025-01-29 15:23:20 +0530

    Create tailwind.config.js

---
+++ weather-frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // Enable dark mode
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        pricedown: ['Pricedown', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
